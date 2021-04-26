20033560d7b70226ad97900dbfc1b32bc28dcd54
LHJE <theeluke@gmail.com>
2021-04-26 10:05:00 -0600

    Update npc facade and npc pdfs controller with spacing corrections

--- app/controllers/npc_pdfs_controller.rb
+++ app/controllers/npc_pdfs_controller.rb
@@ -1,5 +1,4 @@
 class NpcPdfsController < ApplicationController
-
   def show
     @npc = NpcModel.where(id: params[:id])[0]
     @split_spells_slots = @npc.spell_slots.split('{')[1].split('}')[0].split(',').map { |x| x[-1] } unless nil
@@ -8,15 +7,14 @@ class NpcPdfsController < ApplicationController
       format.html
       format.pdf do
         render pdf: "NpcPdf No. #{@npc.id}",
-        page_size: 'A4',
-        template: "npc_pdfs/show.html.erb",
-        layout: "pdf.html",
-        orientation: "Portrait",
-        lowquality: true,
-        zoom: 1,
-        dpi: 75
+               page_size: 'A4',
+               template: 'npc_pdfs/show.html.erb',
+               layout: 'pdf.html',
+               orientation: 'Portrait',
+               lowquality: true,
+               zoom: 1,
+               dpi: 75
       end
     end
   end
-
 end
